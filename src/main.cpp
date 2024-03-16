0025e1c97c427ac73b37c38221008b2bc526266b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-03-16 16:21:27 +0700

    Implement reading partition table

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <argparse/argparse.hpp>
 #include <memory>
+#include <osdi.hpp>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 using namespace argparse;
+
+int sector_size;
+fstream image;
+vector<OSDIPartition> partitions;
 
 shared_ptr<const ArgumentParser> parse_args(int argc, char *argv[])
 {
@@ -34,6 +44,63 @@ int main(int argc, char *argv[])
     auto args = parse_args(argc, argv);
     if (!args)
         return 1;
+    
+    image.open(args->get("image"), ios::in | ios::out | ios::binary);
+    if (!image)
+    {
+        cerr << "Failed to open disk image" << endl;
+        return 1;
+    }
+
+    sector_size = args->get<int>("-s");
+    cout << "Reading partition table..." << endl;
+    for (int i = 0; i < sector_size / sizeof(OSDIPartition); ++i)
+    {
+        OSDIPartition partition;
+        image.read(reinterpret_cast<char *>(&partition), sizeof(OSDIPartition));
+        
+        if (*(uint64_t *)&partition.type != 0)
+            partitions.push_back(partition);
+    }
+
+    if (partitions[0].size != 0 || string(partitions[0].type) != "OSDI\xaa\xaa\x55\x55")
+    {
+        cout << "Not an OSDI image. Create new partition table? [y/N] ";
+
+        char c;
+        cin >> c;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        if (c != 'Y' && c != 'y')
+            return 0;
+
+        image.seekp(0);
+        image.write("\0", sector_size);
+        image.seekp(0);
+
+        OSDIPartition signature;
+        signature.start_sector = 1; // Only version 1 is supported
+        signature.size = 0;
+        memcpy(signature.type, "OSDI\xaa\xaa\x55\x55", 8);
+        memset(signature.flags, 0, 3);
+
+        memcpy(signature.name, "osdi-", 5);
+        srand(time(nullptr));
+        for (int i = 0; i < 8; ++i)
+            signature.name[5 + i] = '0' + (rand() % 10);
+        
+        image.write(reinterpret_cast<const char *>(&signature), sizeof(OSDIPartition));
+        partitions.clear();
+        partitions.push_back(signature);
+    }
+    else if (partitions[0].start_sector != 1)
+    {
+        cout << "Unsupported partition table version" << endl;
+        return 1;
+    }
+    else
+    {
+        cout << "Read in " << partitions.size() << " partitions" << endl;
+    }
     
     return 0;
 }
