d359513b3d5186cad8e6e83fc9472b96ac46aa64
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-07-20 17:49:03 +0700

    Add Clap for argument parsing

--- osdiedit/src/main.rs
+++ osdiedit/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// The disk to edit
+    disk: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let file = match File::open(&args.disk) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error opening disk {}: {}", args.disk, e);
+            return;
+        }
+    };
 }
